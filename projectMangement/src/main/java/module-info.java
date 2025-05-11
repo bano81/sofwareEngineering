9851e6388f7939ebf97b25afec092312cf5a9a71
ThatAugustus <s245764@dtu.dk>
2025-05-11 22:14:06 +0200

    Deleted unused project class + package

--- projectMangement/src/main/java/module-info.java
+++ projectMangement/src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module hellofx {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
  
     opens dtu.ProjectManagementApp.businessLogic to javafx.fxml; // Gives access to fxml files
     exports dtu.ProjectManagementApp.businessLogic; // Exports the class inheriting from javafx.application.Application
