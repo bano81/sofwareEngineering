6b9b2bcbe14239f3f1f11d3fb44636d22ea9940b
MaltheJMathiesen <malthejmathiesen@outlook.com>
2025-05-12 15:34:12 +0200

    Editted pom.xml file and deleted

--- projectMangement/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module hellofx {
- 
-    opens dtu.ProjectManagementApp.businessLogic to javafx.fxml; // Gives access to fxml files
-    exports dtu.ProjectManagementApp.businessLogic; // Exports the class inheriting from javafx.application.Application
-}
